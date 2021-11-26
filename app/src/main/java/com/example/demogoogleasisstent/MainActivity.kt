ac5e36f923b8fd744d7ac01abe8d70e04f324305
bangnv <bang.ngo@ntq-solution.com.vn>
2021-11-26 10:55:47 +0700

    custom intent v2

--- app/src/main/java/com/example/demogoogleasisstent/MainActivity.kt
+++ app/src/main/java/com/example/demogoogleasisstent/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.demogoogleasisstent
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -13,19 +15,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     val CAR_PARK = "https://todo.linbd.com/car_park"
-    val NAVIGATION = "https://todo.linbd.com/navigate"
+    val NAVIGATION = "https://todo.linbd.com/bang"
 
     private fun handleEvent(intent: Intent?) {
         intent?.let {
-
+            Log.d("bangnv", intent.data.toString())
             if (intent.data.toString() == CAR_PARK) {
                 Toast.makeText(this, "Open Carpark", Toast.LENGTH_LONG).show()
             } else if (intent.data.toString().contains(NAVIGATION)) {
-                Toast.makeText(this, "navigation to " + intent.data.toString(), Toast.LENGTH_LONG).show()
+
+                val uri = Uri.parse(intent.data.toString())
+                Log.d("bangnv", intent.data.toString())
+                Toast.makeText(this, "navigation to " + uri.getQueryParameter("place"), Toast.LENGTH_LONG).show()
             }
         }
-
-
     }
 
     override fun onNewIntent(intent: Intent?) {
